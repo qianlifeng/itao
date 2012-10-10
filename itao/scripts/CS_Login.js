43de79d1d9d247f7e8d4d7048655d61df6262fcf
qianlifeng <qianlf2008@163.com>
2012-10-10 15:10:34 +0800

    更改登录触发顺序，首先向后台请求用户登录信息再进行登录操作

--- itao/scripts/CS_Login.js
+++ itao/scripts/CS_Login.js
@@ -1,18 +1,24 @@
 ﻿//和background js建立一个长连接
 var port = chrome.extension.connect({name: "tbLoginPort"});
-window.onload = function() {fillForm()};
 
+//向后台发送消息获得用户信息用于登录
+port.postMessage({act: "getUserForLogin"});
+port.onMessage.addListener(function(msg) {
+    if (msg.act == "startFillForm"){
+        fillForm(msg.user,msg.pwd);
+	}
+});
+	
 //填写登录表单
-function fillForm(){
+function fillForm(user,pwd){
     setTimeout(function() {
                 document.getElementById("J_SafeLoginCheck").checked = false;
                 document.getElementById("J_LongLogin_1").checked = true;
                 setTimeout(function() {
                     var a = document.getElementById("J_StaticForm");
-                    document.getElementById("TPL_username_1").value = 'autorunforscott@163.com';
-                    a.TPL_password.value = 'autorun123456';
+                    document.getElementById("TPL_username_1").value = user;
+                    a.TPL_password.value = pwd;
                     a.submit();
-                    console.log('发送登录完毕请求到login.js');
                     port.postMessage({act: "formSubmitted"});
                 },10);
             },100);
